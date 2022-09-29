489539dd61b086f876aceb25e0b34e30b76aa65c
hisaponz <kitaura.com@gmail.com>
2022-09-29 23:11:14 +0900

    this is test

---
+++ contracts/Inbox.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+contract Inbox {
+    string public message;
+    
+    constructor(string memory initialMessage) public {
+        message = initialMessage;
+    }
+    
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+}
